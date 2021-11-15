3a331eb67d01f71c96957beda4393428aa48d944
Nathan <nathan.forman@gmail.com>
2021-11-15 14:09:37 -0800

    Removed delete from potluck model and got test file working

--- api/potlucks/potlucks-model.js
+++ api/potlucks/potlucks-model.js
@@ -8,14 +8,7 @@ function findById(potluck_id) {
     return db('potlucks').where({ potluck_id }).first()
 }
 
-async function del(potluck_id){
-    const removedPotluck = await findById(potluck_id)
-    await db('potlucks').where({ potluck_id }).del()
-    return removedPotluck
-}
-
 module.exports = {
     find,
-    findById,
-    del
+    findById
 }
